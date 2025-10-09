0d0fc154b2d1257dd3e0541044dfbedd1c37fee0
shakeerjaykay <shakeerjaykay@gmail.com>
2025-10-09 11:23:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,28 @@
-provider "aws" { region = "us-east-1" }
+provider "aws" {
+region = "us-east-1"
+}
 
-locals { env = "test" }
+resource "aws_vpc" "one" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
+}
 
-resource "aws_vpc" "one" { cidr_block = "10.0.0.0/16" tags = { Name = "${local.env}-vpc" } }
+resource "aws_subnet" "two" {
+  vpc_id     = aws_vpc.one.id
+  cidr_block = "10.0.1.0/24"
 
-resource "aws_subnet" "two" { vpc_id = aws_vpc.one.id cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "dev-subent"
+  }
+}
 
-tags = { Name = "${local.env}-subent" } }
-
-resource "aws_instance" "three" { subnet_id = aws_subnet.two.id ami = "ami-0866a3c8686eaeeba" instance_type = "t2.micro" tags = { Name = "${local.env}--server" } }
+resource "aws_instance" "three" {
+  subnet_id     = aws_subnet.two.id
+  ami           = "ami-ami-052064a798f08f0d3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-server"
+  }
+}
